0da3d879e334965e7f1c826e639194f3966b6f6e
“luongthao” <anhthaopro102@gmail.com>
2022-06-22 10:01:34 +0700

    merge brand master

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -1,32 +1,34 @@
 package com.example.controller;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+import com.example.dto.LoginDTO;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-import com.example.dto.RegisterRequest;
-import com.example.service.AuthenService;
-
-import lombok.RequiredArgsConstructor;
-
-@Controller
-@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api")
 public class LoginController {
 
+	private final TokenProvider tokenProvider;
+
+	public LoginController(TokenProvider tokenProvider) {
+		this.tokenProvider = tokenProvider;
+	}
+
 	@GetMapping("/get-user-info")
-	@ResponseBody
     public String currentUserNameSimple() {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 //		System.out.println(SecurityContextHolder.getContext().getAuthentication().getName().toString());
 		return authentication.getName();
     }
+
+    @PostMapping("/authenticate")
+	public String authenticateUser(@Valid @RequestBody LoginDTO loginDTO){
+
+
+		return null;
+
+	}
 }
--- src/main/java/com/example/controller/UserJwtController.java
+++ src/main/java/com/example/controller/UserJwtController.java
@@ -1,20 +1,14 @@
 package com.example.controller;
 
 import com.example.dto.UserDTO;
-import com.example.security.jwt.TokenProvider;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.rmi.ServerException;
 
 @RestController
 @RequestMapping("/api")
